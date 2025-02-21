44eae8e361335302504dc295c995ed7a328259a0
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-02-21 16:47:54 +0530

    update 1.1

---
+++ app/campaigns/[campaign_id]/page.tsx
@@ -0,0 +1,8 @@
+export default function CampaignPage() {
+    return (
+        <div className="px-32 py-28">
+            <p>Campaign x</p>
+        </div>
+    );
+
+}
---
+++ app/campaigns/page.tsx
@@ -0,0 +1,8 @@
+export default function CampaignsPage() {
+    return (
+        <div className="px-32 py-28">
+            <p>This is the Campaigns Page</p>
+        </div>
+    );
+
+}
