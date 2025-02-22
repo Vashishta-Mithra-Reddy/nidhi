0d215b3ecb993140f0d0dc443b6a64bcbb5ef302
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-02-22 06:51:41 +0530

    ui fix 4

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-full py-6 text-center text-white px-24">
-      <div className="h-52 rounded-3xl w-full px-12 py-5 flex items-center justify-between bg-gray-400/60">
+      <div className="h-52 rounded-3xl w-full px-12 py-5 flex items-center justify-between bg-gray-400/60 bg-[url('/graidentx.png')] bg-cover bg-start">
       <p className="font-samarkan text-left text-8xl text-white">nidhi</p>
       <p className="text-white text-lg">© 2025 <span className="font-samarkan">nidhi</span>. All rights reserved.</p>
       </div>
